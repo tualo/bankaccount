d496030f0a80193939c5df46baaa61bfaa4be52a
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-03 09:11:02 +0200

    fix js loader

--- src/Routes/JsLoader.php
+++ src/Routes/JsLoader.php
@@ -1,18 +1,28 @@
 <?php
+
 namespace Tualo\Office\BankAccount\Routes;
 
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
+use Tualo\Office\Basic\RouteSecurityHelper;
 use Tualo\Office\Basic\IRoute;
 
-class JsLoader implements IRoute{
-    public static function register(){
-        BasicRoute::add('/jsbankaccount/(?P<file>[\w.\/\-]+).js',function($matches){
+class JsLoader implements IRoute
+{
+    public static function register()
+    {
+        BasicRoute::add('/jsbankaccount/(?P<file>[\w.\/\-]+).js', function ($matches) {
+            RouteSecurityHelper::serveSecureStaticFile(
+                $matches['file'],
+                dirname(__DIR__, 1) . '/js',
+                ['js'],
+                ['application/javascript']
+            );
+            /*
             App::contenttype('application/javascript');
             readfile( dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js' );
             exit();
-            
-        },['get'],false);
-
+            */
+        }, ['get'], false);
     }
 }
